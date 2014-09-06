6431c27adc9a760304a735473e15734c767f94a8
Paulo Schneider <paulo.schneider@gmail.com>
2014-09-06 23:14:43 +0100

    Add compojure

--- project.clj
+++ project.clj
@@ -3,4 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.1.8"]]
+  :plugins [[lein-ring "0.8.11"]]
+  :ring {:handler silly-image-store.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}})
