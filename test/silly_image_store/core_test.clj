726c821ca2a377acee806e5fabd7978ff735c90b
Paulo Schneider <paulo.schneider@gmail.com>
2014-09-07 10:43:12 +0100

    Delete unused default files

--- test/silly_image_store/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns silly-image-store.core-test
-  (:require [clojure.test :refer :all]
-            [silly-image-store.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
