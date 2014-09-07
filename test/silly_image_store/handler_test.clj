b18f6dfc653f8e823f901334b2808fcfd06de5b1
Paulo Schneider <paulo.schneider@gmail.com>
2014-09-07 20:46:19 +0100

    Organize app test

--- test/silly_image_store/handler_test.clj
+++ test/silly_image_store/handler_test.clj
@@ -4,17 +4,18 @@
             [ring.mock.request :as mock]))
 
 (deftest test-app
-  (testing "serve image"
-    (let [response (app (mock/request :get "/image/zelda.png"))
-          body (:body response)]
-      (is (= (:status response) 200))  
-      (is (= (class body) java.io.File))  
-      (is (= (.getPath body) "resources/images/zelda.png"))))
+  (testing "view image route"
+    (testing "serve image"
+      (let [response (app (mock/request :get "/image/zelda.png"))
+            body (:body response)]
+        (is (= (:status response) 200))  
+        (is (= (class body) java.io.File))  
+        (is (= (.getPath body) "resources/images/zelda.png"))))
 
-  (testing "image not-found"
-    (let [response (app (mock/request :get "/image/not-such-image.png"))]
-      (is (= (:status response) 404))))
+    (testing "image not-found"
+      (let [response (app (mock/request :get "/image/not-such-image.png"))]
+        (is (= (:status response) 404)))))
 
-  (testing "not-found route"
+  (testing "generic not-found route"
     (let [response (app (mock/request :get "/invalid"))]
       (is (= (:status response) 404)))))
