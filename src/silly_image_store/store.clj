fd12ad4b078b26dec5c22b9df39d8ca1047eeb4b
Paulo Schneider <paulo.schneider@gmail.com>
2014-09-08 22:19:54 +0100

    load-image take variable number of paths

--- src/silly_image_store/store.clj
+++ src/silly_image_store/store.clj
@@ -1,10 +1,10 @@
 (ns silly-image-store.store
   (:require [clojure.java.io :as io]))
 
-(defn load-image [basedir filename]
-  (let [file (io/file (str basedir filename))]
+(defn load-image [& paths]
+  (let [file (io/file (apply str paths))]
     (if (.exists file) file nil)))
 
 (defn list-images [basedir]
-  (let [files (.listFiles (io/file basedir))]
+  (let [files (.listFiles (load-image basedir))]
     (map #(.getName %) files)))
