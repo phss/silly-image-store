27766f556a9f1b51bd1aa585b2ba3c954a1771e3
Paulo Schneider <paulo.schneider@gmail.com>
2014-09-14 22:29:21 +0100

    Change serve image route fn to handle request

--- src/silly_image_store/handler.clj
+++ src/silly_image_store/handler.clj
@@ -13,19 +13,19 @@
   (route/not-found (str "No image '" image "' found")))
 
 ; TODO refactor links stuff
-(defn list-images [{scheme :scheme, {host "host"} :headers}]
+(defn- list-images-route [{scheme :scheme, {host "host"} :headers}]
   (let [image-names (store/list-images images-dir)
         base-url (str (name scheme) "://" host "/images/")
         to-json (fn [n] {:name n :link (str base-url n)})]
     (map to-json image-names)))
 
-(defn serve-image [image]
+(defn- serve-image-route [{{image :image} :params}]
   (let [image-file (store/load-image images-dir image)]
     (or image-file (image-not-found image))))
 
 (defroutes app-routes
-  (GET "/images" request list-images)
-  (GET "/images/:image" [image] (serve-image image)) ; TODO handler as list-image
+  (GET "/images" request list-images-route)
+  (GET "/images/:image" request serve-image-route)
   (route/resources "/")
   (route/not-found "Not Found"))
 
