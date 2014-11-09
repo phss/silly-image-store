7ab160f9ba65094c00e2d699b23a7ec7f907c004
Paulo Schneider <paulo.schneider@gmail.com>
2014-11-09 19:30:30 +0000

    Wrap java functions to a local function

--- src/silly_image_store/store.clj
+++ src/silly_image_store/store.clj
@@ -1,13 +1,19 @@
 (ns silly-image-store.store
   (:require [clojure.java.io :as io]))
 
+(def file? #(.isFile %))
+(def filename #(.getName %))
+
 (defn load-image [& paths]
   (let [file (apply io/file paths)]
     (if (.exists file) file nil)))
 
+
 (defn list-images [basedir]
-  (let [files (.listFiles (load-image basedir))]
-    (map #(.getName %) (filter #(.isFile %) files))))
+  (->> (load-image basedir)
+       .listFiles
+       (filter file?)
+       (map filename)))
 
 (defn random-image [basedir]
   (let [random-image-name (rand-nth (list-images basedir))]
