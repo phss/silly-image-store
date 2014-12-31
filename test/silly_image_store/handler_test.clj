072022df9c53a577c174378e23b113b33eb5c902
Paulo Schneider <paulo.schneider@gmail.com>
2014-12-31 13:59:00 -0200

    Create helper function to perform get request in test

--- test/silly_image_store/handler_test.clj
+++ test/silly_image_store/handler_test.clj
@@ -4,9 +4,11 @@
             [silly-image-store.handler :refer :all]
             [ring.mock.request :as mock]))
 
+(def do-get #(app (mock/request :get %)))
+
 (deftest test-app
   (testing "root route"
-    (let [response (app (mock/request :get "/"))
+    (let [response (do-get "/")
           json-body (json/read-str (:body response))]
         (is (= (:status response) 200))  
         (is (= json-body {"images" "http://localhost/images",
@@ -14,50 +16,50 @@
                           "random" "http://localhost/random"}))))
 
   (testing "bucket root route"
-    (let [response (app (mock/request :get "/buckets/some-bucket"))
+    (let [response (do-get "/buckets/some-bucket")
           json-body (json/read-str (:body response))]
         (is (= (:status response) 200))  
         (is (= json-body {"images" "http://localhost/buckets/some-bucket/images",
                           "random" "http://localhost/buckets/some-bucket/random"}))))
 
   (testing "bucket not found"
-    (let [response (app (mock/request :get "/buckets/no-such-bucket"))]
+    (let [response (do-get "/buckets/no-such-bucket")]
       (is (= (:status response) 404))   
       (is (= (:body response) "No thing 'no-such-bucket' found")))) 
 
   (testing "view image route"
     (testing "serve image"
-      (let [response (app (mock/request :get "/images/tubes.jpg"))
+      (let [response (do-get "/images/tubes.jpg")
             body (:body response)]
         (is (= (:status response) 200))  
         (is (= (class body) java.io.File))  
         (is (= (.getPath body) "test/fixtures/tubes.jpg"))))
 
     (testing "serve image from bucket"
-      (let [response (app (mock/request :get "/buckets/some-bucket/images/boxes.jpg"))
+      (let [response (do-get "/buckets/some-bucket/images/boxes.jpg")
             body (:body response)]
         (is (= (:status response) 200))  
         (is (= (class body) java.io.File))  
         (is (= (.getPath body) "test/fixtures/some-bucket/boxes.jpg"))))
 
     (testing "image not-found"
-      (let [response (app (mock/request :get "/images/not-such-image.png"))]
+      (let [response (do-get "/images/not-such-image.png")]
         (is (= (:status response) 404))   
         (is (= (:body response) "No thing 'not-such-image.png' found"))))
     
     (testing "bucket image not-found"
-      (let [response (app (mock/request :get "/buckets/some-bucket/images/not-such-image.png"))]
+      (let [response (do-get "/buckets/some-bucket/images/not-such-image.png")]
         (is (= (:status response) 404))   
         (is (= (:body response) "No thing 'not-such-image.png' found")))))
 
   (testing "serve random image"
-    (let [response (app (mock/request :get "/random"))
+    (let [response (do-get "/random")
           body (:body response)]
       (is (= (:status response) 200))  
       (is (= (class body) java.io.File))))
 
   (testing "serve random image from bucket"
-    (let [response (app (mock/request :get "/buckets/some-bucket/random"))
+    (let [response (do-get "/buckets/some-bucket/random")
           body (:body response)]
       (is (= (:status response) 200))  
       (is (= (class body) java.io.File))))
@@ -65,7 +67,7 @@
     
   (testing "listing images"
     (testing "images from base dir as json"
-      (let [response (app (mock/request :get "/images"))
+      (let [response (do-get "/images")
             json-body (json/read-str (:body response))]
         (is (= (:status response) 200))  
         (is (= json-body 
@@ -75,7 +77,7 @@
                  "url" "http://localhost/images/tubes.jpg"}]))))
     
     (testing "images from bucket as json"
-      (let [response (app (mock/request :get "/buckets/some-bucket/images"))
+      (let [response (do-get "/buckets/some-bucket/images")
             json-body (json/read-str (:body response))]
         (is (= (:status response) 200))  
         (is (= json-body 
@@ -86,12 +88,12 @@
                 ]))))
     
     (testing "bucket not found"
-      (let [response (app (mock/request :get "/buckets/no-such-bucket/images"))]
+      (let [response (do-get "/buckets/no-such-bucket/images")]
         (is (= (:status response) 404))   
         (is (= (:body response) "No thing 'no-such-bucket' found")))))
 
   (testing "listing buckets"
-    (let [response (app (mock/request :get "/buckets"))
+    (let [response (do-get "/buckets")
           json-body (json/read-str (:body response))]
       (is (= (:status response) 200))
       (is (= json-body [{"name" "some-bucket",
@@ -99,5 +101,5 @@
 
 
   (testing "generic not-found route"
-    (let [response (app (mock/request :get "/invalid"))]
+    (let [response (do-get "/invalid")]
       (is (= (:status response) 404)))))
